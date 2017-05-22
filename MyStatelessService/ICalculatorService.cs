0c464f7b31bc60cdd2608008d60d2bfd56abe84e
alex Gordon <aleksandr.gordon@gmail.com>
2017-05-22 14:44:34 -0500

    Testing commit

--- MyStatelessService/ICalculatorService.cs
+++ MyStatelessService/ICalculatorService.cs
@@ -6,6 +6,6 @@ namespace MyStatelessService
     public interface ICalculatorService:IService
     {
         Task<string> Add(int a, int b);
-        Task<string> Subtract(int a, int b);
+        Task<string> Subtract(int a, int b); 
     }
 }
