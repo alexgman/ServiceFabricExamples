bfad1962d6ad91df1db7d3152b0fcd28cdc24c1d
alex Gordon <aleksandr.gordon@gmail.com>
2017-05-22 14:41:23 -0500

    Initial commit

---
+++ CalculatorClient/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyStatelessService;
+using Microsoft.ServiceFabric.Services.Remoting.Client;
+using System.Threading;
+
+namespace CalculatorClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                var calculatorClient = ServiceProxy.Create<ICalculatorService>
+                        (new Uri($"fabric:/CalculatorService/MyStatelessService"));  //service locator
+                var result = calculatorClient.Add(1, 2).Result;
+                Console.WriteLine(result);
+                Thread.Sleep(3000);
+            }
+
+        }
+    }
+}
---
+++ MyStatelessService/CalculatorService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using Microsoft.ServiceFabric.Services.Remoting.Runtime;
+using Microsoft.ServiceFabric.Services.Runtime;
+using System.Collections.Generic;
+using System.Fabric;
+using System.Threading.Tasks;
+
+namespace MyStatelessService
+{
+    internal sealed class CalculatorService : StatelessService, ICalculatorService
+    {
+        public CalculatorService(StatelessServiceContext serviceContext) : base(serviceContext)
+        {
+        }
+
+        public Task<string> Add(int a, int b)
+        {
+            return Task.FromResult(string.Format("Instance{0} returns: {1}", base.Context.InstanceId, a + b));
+        }
+
+        public Task<string> Subtract(int a, int b)
+        {
+            return Task.FromResult(string.Format("Instance{0} returns: {1}", base.Context.InstanceId, a - b));
+        }
+
+        protected override IEnumerable<ServiceInstanceListener> CreateServiceInstanceListeners()
+        {
+            return new[] { new ServiceInstanceListener(context => this.CreateServiceRemotingListener(context)) };
+        }
+    }
+}
